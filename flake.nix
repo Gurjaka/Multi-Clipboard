91b6908b6420ca31431fc4c9c9b24f2a0574438f
Gurjaka <esartia.gurika@gmail.com>
2024-08-28 20:37:46 +0400

    Batman

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    py = pkgs.python3.pkgs;
+  in 
+  {
+  devShells.${system}.default = 
+  pkgs.mkShell
+    {
+      packages = with pkgs; [
+        python3
+        fish
+        py.pyperclip
+      ];
+
+      shellHook = ''
+        exec fish
+      '';
+    };
+  };
+}
+
