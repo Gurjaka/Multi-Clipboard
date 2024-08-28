5da7d0f9b4e64143945ca690047911f2a13644f0
Gurjaka <esartia.gurika@gmail.com>
2024-08-28 21:10:04 +0400

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
         'upsell': """Would you consider making this a monthly donation?"""}
 
 if len(sys.argv) < 2:
-    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    print('Usage: py mclip.py [keyphrase] - copy phrase text')
     sys.exit()
 
 keyphrase = sys.argv[1]    # first command line arg is the keyphrase
